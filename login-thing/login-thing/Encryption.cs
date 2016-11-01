4e773e28785400443d712920926331633ee28aa0
Luke Woodall <luke_woodall@live.co.uk>
2016-11-01 22:55:43 +0000

    Pretty much done tbf

---
+++ login-thing/login-thing/Encryption.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Security.Cryptography;
+
+namespace login_thing
+{
+    /// <summary>
+    /// This class is an industry standard method of encryption, I didn't write it but it basically generates a random bunch of bytes called a salt
+    /// and uses that to encrypt the password 4096 times, it's one way, so to test if the user put in the right password you have to encrypt the
+    /// password they give you and then compared that against the stored one.
+    /// </summary>
+    public class PasswordHash 
+    {
+        // The following constants may be changed without breaking existing hashes.
+        public const int SALT_BYTE_SIZE = 24;
+        public const int HASH_BYTE_SIZE = 24;
+        public const int PBKDF2_ITERATIONS = 4096;
+
+        public const int ITERATION_INDEX = 1;
+        public const int SALT_INDEX = 2;
+        public const int PBKDF2_INDEX = 3;
+
+        // Creates a salted PBKDF2 hash of the password.
+        public static string CreateHash(string password)
+        {
+            // Generate a random salt
+            RNGCryptoServiceProvider csprng = new RNGCryptoServiceProvider();
+            byte[] salt = new byte[SALT_BYTE_SIZE];
+            csprng.GetBytes(salt);
+
+            // Hash the password and encode the parameters
+            byte[] hash = PBKDF2(password, salt, PBKDF2_ITERATIONS, HASH_BYTE_SIZE);
+            return "PBKDF2:" + PBKDF2_ITERATIONS + ":" + Convert.ToBase64String(salt) + ":" + Convert.ToBase64String(hash);
+        }
+
+        // Validates a password given a hash of the correct one.
+        public static bool ValidatePassword(string password, string correctHash)
+        {
+            // Extract the parameters from the hash
+            char[] delimiter = { ':' };
+            string[] split = correctHash.Split(delimiter);
+            int iterations = Int32.Parse(split[ITERATION_INDEX]);
+            byte[] salt = Convert.FromBase64String(split[SALT_INDEX]);
+            byte[] hash = Convert.FromBase64String(split[PBKDF2_INDEX]);
+            byte[] testHash = PBKDF2(password, salt, iterations, hash.Length);
+            return SlowEquals(hash, testHash);
+        }
+
+        // Compares two byte arrays in length-constant time. This comparison
+        // method is used so that password hashes cannot be extracted from
+        // on-line systems using a timing attack and then attacked off-line.
+        private static bool SlowEquals(byte[] a, byte[] b)
+        {
+            uint diff = (uint)a.Length ^ (uint)b.Length;
+            for (int i = 0; i < a.Length && i < b.Length; i++)
+            {
+                diff |= (uint)(a[i] ^ b[i]);
+            }
+            return diff == 0;
+        }
+
+        // Computes the PBKDF2-SHA1 hash of a password.
+        private static byte[] PBKDF2(string password, byte[] salt, int iterations, int outputBytes)
+        {
+            Rfc2898DeriveBytes pbkdf2 = new Rfc2898DeriveBytes(password, salt);
+            pbkdf2.IterationCount = iterations;
+            return pbkdf2.GetBytes(outputBytes);
+        }
+    }
+}
--- login-thing/login-thing/MainWindow.xaml.cs
+++ login-thing/login-thing/MainWindow.xaml.cs
@@ -7,7 +7,7 @@ using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Data;
-using System.Windows.Documents;
+using System.Windows.Documents; //I don't need half of these. Oh well.
 using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
@@ -18,6 +18,7 @@ using System.Data.Linq;
 using System.Data.Sql;
 using System.Data.Linq.Mapping;
 using System.Text.RegularExpressions;
+using System.Xml.Serialization;
 
 namespace login_thing
 {
@@ -27,6 +28,7 @@ namespace login_thing
     ///create a base class that both of those inherit from to avoid writing the same code twice.     
     /// </summary>
 
+    //[INTERFACE STUFF]-----------------------------------------------------------------------------------------------
     public class BaseNotify : INotifyPropertyChanged //The colon here means that this class inherits from the interface INotifyPropertyChanged
     {
         public event PropertyChangedEventHandler PropertyChanged; //This is the implementation of that interface with the event PropertyChanged
@@ -41,14 +43,40 @@ namespace login_thing
         }
     }
 
+    public class Command : ICommand //This class is needed so that the functions I write here can interact with the gui, you can pretty much ignore it
+    {
+        public event EventHandler CanExecuteChanged;
+
+        private Action command;
+
+        public Command(Action command)
+        {
+            this.command = command;
+        }
+
+        public bool CanExecute(object parameter)
+        {
+            return true;
+        }
+
+        public void Execute(object parameter)
+        {
+            if (command != null) command();
+        }
+    }
+    //[/INTERFACE STUFF]-----------------------------------------------------------------------------------------------
+
+    //[SIGNUP/LOGIN STUFF]--------------------------------------------------------------------------------------------------
     public class SignUp : BaseNotify //As you can see here, this class inherits from the BaseNotify class I wrote earlier, allowing it to use everything in that class
     {        
         //These statements are properties for the SignUp class, these will retrieve the appropriate bits of data from the gui
+        //A property in C# is essentially a function that returns a value, it has a getter and a setter, in WPF, GUI elements can only interact
+        //with properties, not variables, hence why we have separate variables for storing the data once it has been retrieved.
         public string forename { get { return _forename; } set { _forename = value; Notify("forename"); } }
-        public string surname { get { return surname; } set { _surname = value; Notify("surname"); } }
-        public string username { get { return username; } set { _username = value; Notify("username"); } }
-        public string email { get { return email; } set { _email = value; Notify("email"); } }
-        public string password { get { return password; } set { _password = value; Notify("password"); } }
+        public string surname { get { return _surname; } set { _surname = value; Notify("surname"); } }
+        public string username { get { return _username; } set { _username = value; Notify("username"); } }
+        public string email { get { return _email; } set { _email = value; Notify("email"); } }
+        public string password { get { return _password; } set { _password = value; Notify("password"); } }
 
         //These variables are what the data will be stored in when it is retrieved
         private string _forename;
@@ -56,12 +84,23 @@ namespace login_thing
         private string _username;
         private string _email;
         private string _password;
+        
+        //What I'm doing here is the same as what I did earlier with the properties forename, surname etc, only here I'm doing it with the function
+        //that will execute when a button is pressed.
+        public Command SignUpCommand {  get { return _signUpCommand; } }
+        private Command _signUpCommand;
 
-        public ICommand<void> signUp
+        public SignUp() //This is the class initializer, it creates an instance of the command class I made earlier and puts in the signUp method
+        {               //as a parameter.
+            _signUpCommand = new Command(signUp);
+        }
 
-        public void signUp()
-        {
-            UserDB db = new UserDB(AppDomain.CurrentDomain.BaseDirectory + @"\users.mdf");
+        public void signUp() //This is the signUp function which executes when the signup button is pressed.
+        {                    //Here, public is the protection level, public meaning it can be accessed from outside of this class
+                             //The Void keyword is the return type of the function, this function doesn't return anything, so void is used.
+            UserList curFile = new UserList(); //This creates a new instance of the UserList class and assigns it to the variable "curFile"
+            //This block here creates a new instance of the user class, but as you can see, it sets the values of the variables in that class
+            //to the data that was retrieved from the GUI.
             User newUser = new User
             {
                 username = _username,
@@ -71,13 +110,26 @@ namespace login_thing
                 email = _email
             };
             
-            //TODO: Presence check
+            //This is one big if statement that checks if any of the data retrieved from the text boxes is an empty string or null.
+            //Here, the || operator is equivalent to "or" in python
+            if (string.IsNullOrEmpty(newUser.username) == true || 
+                string.IsNullOrEmpty(newUser.password) == true || 
+                string.IsNullOrEmpty(newUser.forename) == true ||
+                string.IsNullOrEmpty(newUser.surname) == true ||
+                string.IsNullOrEmpty(newUser.email) == true)
+            {
+                MessageBox.Show("You must fill all fields.");
+                return;
+            }
 
+            //This is a length check for the username.
             if (newUser.username.Length > 10)
             {
                 MessageBox.Show("The username must be no longer than 10 characters.");
-                return;
+                return; //This return statement at the end of all the ifs basically exits out of the function
             }
+            //All of these next validation checks are done using regex, I used regex because it lets me do type checks, format checks and length
+            //checks all in one go, making the code much neater.
             if (!Regex.IsMatch(newUser.password, @"^(?=.*[A-Z])(?=.*\d)[a-zA-Z0-9]{6,12}$"))
             {
                 MessageBox.Show("The password must be between 6 and 12 characters and contain at least one number and one capital letter.");
@@ -99,81 +151,146 @@ namespace login_thing
                 MessageBox.Show("Please enter a valid email");
                 return;
             }
+
+            //Here I've retrieved the users from the file
+            List<User> tempUsers = curFile.LoadDatabase();
+            foreach (User x in tempUsers) //This for loop combined with an if statement checks if the user being added has the same username as
+            {                             //an existing user.
+                if (x.username == _username)
+                {
+                    MessageBox.Show("A user with that username already exists.");
+                    return;
+                }
+            }
+
+            string encryptedPass = PasswordHash.CreateHash(newUser.password); //This encrypts the password and replaces the original with the new
+            newUser.password = encryptedPass; //encrypted one, the code for this is in Encryption.cs
+
+            //If the data passed all the checks then it is added to the list of users
+            curFile.users.Add(newUser);
+
+            //Then is is saved
+            curFile.SaveDatabase();
+            //Then the text fields are cleared
+            clear();
+        }
+
+        public void clear() //Because the data binding is two way, if I set the username, forename etc properties to something, the text in the
+        {                   //textboxes will also change.
+            username = "";
+            forename = "";
+            surname = "";
+            email = "";
+            password = "";
         }
     }
 
-    public class Login : BaseNotify
+    public class Login : BaseNotify //This class is similar to the SignUp class but for logging in
     {
+        private Command _loginCommand;
+        public Command loginCommand { get { return _loginCommand; } }//Here we have the command stuff I mentioned earlier
 
-        public string username { get { return username; } set { _username = value; Notify("username"); } }
-        public string password { get { return password; } set { _password = value; Notify("password"); } }
+        public Login()//Init
+        {
+            _loginCommand = new Command(login);
+        }
 
-        private string _username;
+        public string username { get { return _username; } set { _username = value; Notify("username"); } }//Properties
+        public string password { get { return _password; } set { _password = value; Notify("password"); } }
+
+        private string _username;//Storing vars
         private string _password;
-    }
-
-    //<DATABASE STUFF------------------------------------------------------------------------------------------------>
-    public class UserDB : DataContext
-    {
-        public Table<User> Users;
-        public UserDB(string connection) : base(connection) { }
-
-        public void addUser(User user)
+        
+        public void login()//Login function
         {
-            return;
+            UserList curFile = new UserList();
+            List<User> curUsers = curFile.LoadDatabase();
+
+            foreach (User x in curUsers)
+            {
+                if (x.username == _username && PasswordHash.ValidatePassword(_password, x.password) == true)//Checks if the username and password match anything in the file
+                {
+                    MessageBox.Show("Welcome "+x.forename+", You have logged in successfully.");
+                    return;
+                }
+            }
+
+            MessageBox.Show("Username/password is incorrect or the user doesn't exist.");
+        }
+    }
+    //[/SIGNUP/LOGIN STUFF]-----------------------------------------------------------------------------------------------
+
+    //[FILE STUFF]----------------------------------------------------------------------------------------------------
+    public class UserList //Class for all the saving and loading to file. I was going to use SQL but it's a massive hassle in C#, needs servers and things
+    {
+        public string filePath;
+
+        public List<User> users;
+
+        public UserList()
+        {
+            filePath = AppDomain.CurrentDomain.BaseDirectory + "users.xml"; //Gets the current file path of the program's exe
+
+            users = new List<User>();
         }
 
-        public void checkUser(User user)
+        public List<User> LoadDatabase()//Loads the data from file, note how the return type here is "List<User>" meaning all the possible outcomes
+        {                               //of this method must return a list of user objects
+            if (!File.Exists(filePath))
+            {
+                users = new List<User>();
+                return users;
+            }
+            else
+            {
+                XmlSerializer serialiser = new XmlSerializer(typeof(List<User>)); //New instance of the .NET XmlSerializer
+                using (StreamReader sr = new StreamReader(filePath)) //This using thing means that the StreamReader is disposable, meaning that
+                {                                                    //once it's finished doing what it's doing, the object is disposed of
+                                                                     //meaning that we won't have multiple things trying to access the same file
+
+                    users = (List<User>)serialiser.Deserialize(sr); //This right here is straight up magic, it takes the XML data and turns it into
+                }                                                   //a list of user objects
+
+                return users; //Return the list
+            }
+        }
+
+        public void SaveDatabase() //This does the opposite of the load method, takes a list and converts it to XML data
         {
-            return;
+            XmlSerializer serialiser = new XmlSerializer(typeof(List<User>));
+            //serialiser.Serialize(new StreamWriter(filePath), users);
+            using (StreamWriter sr = new StreamWriter(filePath))
+            {
+                serialiser.Serialize(sr, users);
+            }
         }
     }
-
-    [Table(Name = "UserTable")]
-    public class User
-    {
-        [Column(IsPrimaryKey = true)]
-        public string username;
-        [Column]
-        public string password;
-        [Column]
-        public string forename;
-        [Column]
-        public string surname;
-        [Column]
+    
+    public class User //My user class, which is used to store data for each user.
+    {        
+        public string username;     
+        public string password;        
+        public string forename;        
+        public string surname;        
         public string email;
     }
+    //[/FILE STUFF]-------------------------------------------------------------------------------------------------
 
-    public class Repository
-    {
-        public void addUser (User user, UserDB db)
-        {
-            return;
-        }
-
-        public void checkUser (User user)
-        {
-            return;
-        }
-    }
-    //</DATABASE STUFF------------------------------------------------------------------------------------------------>
-
-    public partial class MainWindow : Window
+    public partial class MainWindow : Window //This is the class that deals with GUI things
     {
         public Login login { get; set; }//This sets the login class as a property of the mainwindow class
         public SignUp signUp { get; set; }//This does the same for the SignUp class
         public MainWindow()
         {
-            DataContext = new SignUp(); //The fact that the other two classes are properties of this class allow me to set this class as the data context of itself
+            signUp = new SignUp(); //Initializes the SignUp and Login classes allowing the GUI to use them
+            login = new Login();
+
+
+            DataContext = this; //The fact that the other two classes are properties of this class allow me to set this class as the data context of itself
                                 //This means that I can specify whether a gui element is for Signing Up or Logging in by putting the data context as
                                 //login.whatever or signup.whatever            
-            string[] dir = Directory.GetFiles(AppDomain.CurrentDomain.BaseDirectory);
-            if (dir.Contains(AppDomain.CurrentDomain.BaseDirectory+"users.mdf") == false)
-            {
-                UserDB db = new UserDB(@"Data Source=(LocalDB)\MSSQLLoc alDB;AttachDbFilename=C:\Users\luke_\Source\Repos\Login-Thing\login-thing\login-thing\bin\Debug\users.mdf;Integrated Security=True");
-                db.CreateDatabase();
-            }
-            InitializeComponent();
-        }
+            
+            InitializeComponent(); //This was generated by Visual Studio, I don't know what it does, it's probably evil.
+        }        
     }
 }
